8b79973e793349153c77221c7545ebbac9344674
amir <amiralizadeh9480@gmail.com>
2019-06-08 13:55:39 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Flutter widget wrap
+
+Wrap selection of flutter code inside Container/Stack.
+I got idea from this extension. [htmltagwrap](https://marketplace.visualstudio.com/items?itemName=bradgashler.htmltagwrap)
+
+![Screen Shot](/images/screenshot.gif)
+
+## How to use
+* Select one or more block of widgets.
+* Press `Alt + C` to wrap inside `Container` or press `Alt + S` to wrap inside `Stack`.
+* Change class name to whatever you want.
+
+## Recent changes
+### 1.0.0
+Initial release
+
---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+
+function getSpacer() {
+	const editor = vscode.window.activeTextEditor;
+	if (editor && editor.options.insertSpaces) {
+		return ' '.repeat(<number>editor.options.tabSize);
+	}
+	return '\t';
+}
+
+function insertSnippet(before: string, after: string, space: string) {
+	const editor = vscode.window.activeTextEditor;
+	if (editor && editor.selection.start !== editor.selection.end) {
+		var selection = editor.selection;
+		var child =  editor.document.getText(selection).trimLeft();
+		var line = editor.document.lineAt(selection.start);
+		child = child.replace(new RegExp("\n\\s{" + line.firstNonWhitespaceCharacterIndex + "}", "gm"), "\n" + space);
+		var replaceText = before + child + after;
+		if (child.substr(-1) === ",") {
+			replaceText += ",";
+		}
+		editor.insertSnippet(new vscode.SnippetString(replaceText), selection);
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('fww.wrapInContainer', () =>
+		insertSnippet("${1:Container}(\n" + getSpacer() + "child: $2", "\n)", getSpacer())
+	));
+	context.subscriptions.push(vscode.commands.registerCommand('fww.wrapInStack', () =>
+		insertSnippet("${1:Stack}(\n" + getSpacer() + "children: [\n" + getSpacer().repeat(2), "$2\n" + getSpacer() + "]\n)", getSpacer().repeat(2))
+	));
+}
